80239adfb6c2c9e1d169f5afe3b00fd0d60fcedc
tintin400819 <tintin.400819@gmail.com>
2025-02-12 10:51:21 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+# Don't use quotes(  and  )
+# SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+
+# Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7558566671:AAHwI7_ldXWZFy09dTB2mO2XXpX-8PJoYo8")
+
+# Enter Your telegram usernames here without @
+OWNER_USERNAME=("@Ayush143", "@ClutchGodOp")
+
+# Enter your admin IDs here. Get it from @missRose_bot by typing /info
+ADMIN_IDS=("859818729", "787517301")
